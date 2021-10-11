c7f6a4dc19dc50bc5cc22e518d6d88de6eb1b7a9
Joseph Palmezano <joseph.palmezano@wizeline.com>
2021-10-11 18:46:20 -0500

    http & serializer_helper fix

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,9 +1,9 @@
-require File.expand_path('helpers/serializer_helper', __dir__)
+require_relative "support/serializer_helper"
 
 class MovieSerializer
   include JSONAPI::Serializer
 
-  include SerializerHelper
+  include ::SerializerHelper
 
   attributes :id, :title, :poster_url, :genres
 
